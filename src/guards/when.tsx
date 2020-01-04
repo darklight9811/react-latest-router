469fa116148c0d4c57405a66dad569b1b6e692f0
Rafael Correa <rafael.chaves98@hotmail.com>
2020-01-04 04:19:40 -0300

    Added code coverage and started travis

--- src/guards/when.tsx
+++ src/guards/when.tsx
@@ -4,8 +4,8 @@ import { iGuardData } from "../interfaces/helpers";
 //Check route validation
 export default function when (_arguments : any[] | null, data : iGuardData) : Object | boolean {
     //No when required
-    if (!("when" in data["route"])) return true;
+    if (!("when" in data.route)) return true;
 
     //Validate
-    return !!data["route"]["when"];
+    return !!data.route["when"];
 }
