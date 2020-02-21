936ba1d20aca4fc4ebc0dfc0775723d5cdb63482
Rafael <rafael.chaves98@hotmail.com>
2020-02-20 23:20:55 -0300

    Added patreon badge

--- src/interfaces/iReact.ts
+++ src/interfaces/iReact.ts
@@ -1,8 +1,11 @@
-import { ReactElement, ReactChildren, ReactChild, ComponentElement, FunctionComponent, Props } from "react";
+import { FunctionComponent, Props } from "react";
 
 export interface iReactProps extends Props<any> {
-	children? : iReactComponent | any,
-	to?       : any
+	children?   : iReactComponent | any,
+  to?         : any,
+
+  //Generic props to allow guards
+  [x:string]  : any,
 }
 
 export interface iReactComponent extends FunctionComponent {
