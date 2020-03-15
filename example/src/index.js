dfae46928a52153004b45fb34503443a7f8fb535
Rafael <rafael.chaves98@hotmail.com>
2020-03-15 14:27:19 -0300

    Removed example github pages

--- example/src/index.js
+++
@@ -1,8 +0,0 @@
-//Core
-import React    from './node/react';
-import ReactDOM from './node/react-dom';
-
-//Application
-import App from './App';
-
-ReactDOM.render(<App />, document.getElementById('root'));
